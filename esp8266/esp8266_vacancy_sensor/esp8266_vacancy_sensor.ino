c0cd7f4d38b1ff298bd233cbde31dc1adcda9b54
Austen HC <plasmarobo@gmail.com>
2018-06-24 13:34:58 -0400

    Version 1.0.0

---
+++ esp8266/esp8266_vacancy_sensor/esp8266_vacancy_sensor.ino
@@ -0,0 +1,105 @@
+#include <EEPROM.h>
+#include <ESP8266WiFi.h>
+#include <limits.h>
+#include <PubSubClient.h>
+#include <string.h>
+
+extern "C" {
+  #include <user_interface.h>
+}
+
+bool doorState = false;
+bool needsPublish = false;
+
+const char* ssid = "vacancy";
+const char* password = "bA24PL0Y";
+
+const char* mqttServer = "vacancy.local";
+const int mqttPort = 1883;
+const char* mqttUser = "vacancy";
+const char* mqttPassword = "vacancy";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+#define SENSOR_PIN 2
+// 10 second sleep cycle
+#define SLEEP_INTERVAL 10000 
+
+unsigned int id = 0xFF000000;
+char mqttID[32];
+char mqttTopic[32];
+
+void setup() {
+  // Check if EEPROM has been initialized
+  Serial.begin(115200);
+  unsigned char id_indicator = EEPROM.read(0);
+  Serial.println("Booting, reading id");
+  if (id_indicator == 0xFF) {
+    while (id >= 0xFF000000) {
+      randomSeed(ESP.getCycleCount());
+      id = random(INT_MAX);
+    }
+    Serial.printf("Generated id: %08x, saving...\n", id);
+    for (int i = 0; i < sizeof(id); ++i) {
+      EEPROM.write((unsigned char) (id >> (sizeof(id) - i - 1)) & 0xFF, i);
+    }
+  } else {
+    id = 0;
+    for (int i = 0; i < sizeof(id); ++i) {
+      id |= EEPROM.read(i) << (sizeof(id) - i - 1);
+    }
+    Serial.printf("Found id %08x\n", id);
+  }
+  sprintf(&mqttID[0], "doorsensor%08x", id);
+  sprintf(&mqttTopic[0], "door/sensor%08x/status", id);
+  client.setServer(mqttServer, mqttPort);
+  Serial.print("Starting wifi...");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("done!");
+  Serial.print("Setting up hardware...");
+  pinMode(SENSOR_PIN, INPUT);
+  attachInterrupt(SENSOR_PIN, doorStateChanged, CHANGE);
+  doorStateChanged();
+  Serial.println("done!");
+}
+
+void doorStateChanged(){
+  doorState = (bool)digitalRead(SENSOR_PIN);
+  needsPublish = true;
+}
+
+void publishState(){
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Connecting WiFi...");
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
+      Serial.print(".");
+    }
+    Serial.println("Connencted");
+  }
+  
+  while(!client.connected()) {
+    Serial.print("Connecting MQTT...");
+    if (!client.connect(mqttID, mqttUser, mqttPassword)){
+      delay(2000);
+      Serial.print(".");
+    } else {
+      Serial.println("Connected");
+    }
+  }
+  client.publish(&mqttTopic[0], (doorState ? "1" : "0"), true);
+  Serial.println("Published update!");
+  needsPublish = false;
+  wifi_set_sleep_type(LIGHT_SLEEP_T);
+}
+
+void loop() {
+  if (needsPublish) {
+    publishState();
+  }
+  delay(SLEEP_INTERVAL);
+}
+
---
+++ pi_leds/vacancy.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+from bibliopixel.drivers.channel_order import ChannelOrder
+from bibliopixel.drivers.spi_interfaces import SPI_INTERFACES
+from bibliopixel.drivers.SPI.LPD8806 import LPD8806
+from bibliopixel.layout import Strip
+import paho.mqtt.subscribe as subscribe
+import re
+import sys
+import time
+
+
+class Door(object):
+    VACANT = 0
+    OCCUPIED = 1
+    VACANT_COLOR = (0,255,0)
+    OCCUPIED_COLOR = (255,0,0)
+    INVALID_COLOR = (0,0,255)
+
+    def __init__(self, sensor, state, start_addr, led_count):
+        self.sensor_name = sensor
+        self.state = state
+        self.start_addr = start_addr
+        self.led_count = led_count
+
+    def getState(self, state):
+        return self.state
+
+    def setState(self, state):
+        self.state = state
+
+    def setLEDs(self, led_buffer):
+        color = Door.INVALID_COLOR
+        if self.state == Door.VACANT:
+            color = Door.VACANT_COLOR
+        if self.state == Door.OCCUPIED:
+            color = Door.OCCUPIED_COLOR
+        print("Using color {}".format(color))
+        for i in range(self.led_count):
+            led_buffer.set(self.start_addr + i, color)
+
+
+class VacancySign(object):
+
+    def __init__(self, host, door_count, leds_per_door):
+        self.host = host
+        self.doors = {}
+        self.door_regex = re.compile(r"door/(sensor[0-9A-F]+)/status")
+        pixel_count = door_count * leds_per_door
+        self.led_driver = LPD8806(num=pixel_count, dev="/dev/spidev0.0", c_order=ChannelOrder.BRG, spi_interface=SPI_INTERFACES.PERIPHERY, spi_speed=2)
+        self.led_driver.set_brightness(128)
+        self.leds = Strip(self.led_driver)
+        self.next_addr = 0
+        self.addr_stride = leds_per_door
+        self.clear()
+
+    def onMessage(self, client, userdata, message):
+        print("{} {}".format(message.topic, message.payload))
+        matches = self.door_regex.match(message.topic)
+        if matches:
+            sensor = matches.group(1)
+            state = int(message.payload)
+            if sensor in self.doors.keys():
+                self.doors[sensor].setState(state)
+            else:
+                self.doors[sensor] = Door(matches.group(1), state, self.next_addr, self.addr_stride)
+                self.next_addr += self.addr_stride
+            self.updateDisplay()
+
+    def updateDisplay(self):
+        self.leds.all_off()
+        for door in self.doors.values():
+            door.setLEDs(self.leds)
+        self.leds.update()
+
+    def run(self):
+        subscribe.callback(self.onMessage, "door/+/status", hostname=self.host)
+
+    def clear(self):
+        self.leds.all_off()
+        self.leds.update()
+
+
+def main():
+    sign = VacancySign("localhost", 3, 2)
+    try:
+        while True:
+            sign.run()
+    except KeyboardInterrupt:
+        sign.clear()
+        sys.exit(0)
+
+main()
